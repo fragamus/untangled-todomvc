d217d8dfd8a1c31294cb06855e9a91b7e402fbf7
Michael Gough <innovative.engineer@gmail.com>
2017-01-01 15:43:06 -0800

    add a main

---
+++ src/server/todomvc/web.clj
@@ -0,0 +1,31 @@
+(ns todomvc.web
+  (:require
+    [clojure.java.io :as io]
+    [clojure.pprint :refer (pprint)]
+    [clojure.stacktrace :refer (print-stack-trace)]
+    [clojure.tools.namespace.repl :refer [disable-reload! refresh clear set-refresh-dirs]]
+    [com.stuartsierra.component :as component]
+    [figwheel-sidecar.repl-api :as ra]
+    [taoensso.timbre :refer [info set-level!] :as timbre]
+    [todomvc.system :as system]
+))
+
+;;SERVER
+
+(def system (atom nil))
+
+(defn init
+  "Create a web server from configurations. Use `start` to start it."
+  []
+  (reset! system (system/make-system)))
+
+(defn start "Start (an already initialized) web server." [] (swap! system component/start))
+
+(defn go "Load the overall web server system and start it." []
+  (init)
+  (start))
+
+(defn -main
+    "Main entry point for the server"
+    [& args]
+    (do (printf "starting app............config=\"%s\"........." (System/getProperty "config"))  (go)) ) 
